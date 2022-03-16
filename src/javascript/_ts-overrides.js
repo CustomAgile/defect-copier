3f3fb3734b99b870f989fd3df2e5455d6f4ab0de
Charles Ferentchak <charles@customagile.com>
2022-03-16 13:49:54 -0500

    Init

---
+++ src/javascript/_ts-overrides.js
@@ -0,0 +1,14 @@
+Ext.override(Rally.ui.dialog.ChooserDialog,{
+    items: {
+        xtype: 'panel',
+        border: false,
+        items: [
+            {
+                xtype: 'container',
+                itemId: 'gridContainer',
+                layout: 'fit'
+            }
+        ]
+    }
+});
+
